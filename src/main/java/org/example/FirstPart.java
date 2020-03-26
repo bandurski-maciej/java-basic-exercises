9e5d48a17bebe78256282ff72efad97f443660c6
maciej <bandurskim@gmail.com>
2020-03-26 14:46:35 +0100

    Initial commit

---
+++ src/main/java/org/example/FirstPart.java
@@ -0,0 +1,167 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class FirstPart {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static <T, E> T getKeyByValue(Map<T, E> map, E value) {
+    for (Map.Entry<T, E> entry : map.entrySet()) {
+      if (Objects.equals(value, entry.getValue())) {
+        return entry.getKey();
+      }
+    }
+    return null;
+  }
+
+  public static Map<String, Long> countCharOccurrence(String text) {
+    return text.toLowerCase().chars()
+      .filter(Character::isLetter)
+      .mapToObj(c -> String.valueOf((char) c))
+      .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+
+  }
+
+  public static String findFindLetterInString(String text) {
+    Map<String, Long> stringLongMap = countCharOccurrence(text);
+    text = text.toLowerCase();
+    String letter = "";
+    char[] chars = text.toCharArray();
+    for (int i = 0; i < text.length(); i++) {
+      if (stringLongMap.get(String.valueOf(chars[i])).equals(1L)) {
+        letter = String.valueOf(text.charAt(i));
+        System.out.println(letter);
+        break;
+      }
+    }
+    return letter;
+  }
+
+  public static boolean checkIfContains(int[] array) {
+    boolean check = false;
+    for (int value : array) {
+      if (value >= 10 && value < 20 || value >= 30 && value < 40) {
+        check = true;
+        break;
+      }
+    }
+    return check;
+  }
+
+  public static boolean checkIfPowerOf(int number) {
+
+    double power = 2.0;
+    double divider = number;
+    boolean check = false;
+
+    while (divider > 1) {
+      check = divider % power == 0;
+
+      divider = divider / power;
+    }
+    return check;
+  }
+
+  public static Integer[] createNewArray(int[] array) {
+    List<Integer> numbers = new ArrayList<>();
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == 10) {
+        for (int j = i; j < array.length - i + 1; j++) {
+          numbers.add(array[j]);
+        }
+        break;
+      }
+    }
+
+    Integer[] returnArray = new Integer[numbers.size()];
+
+    numbers.toArray(returnArray);
+
+    return returnArray;
+  }
+
+  public static Integer[] createNewArrayUpgrade(int[] array) {
+    List<Integer> numbers = new ArrayList<>();
+
+    int indexOfLastNumber = 0;
+
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == 10) {
+        indexOfLastNumber = i;
+      }
+    }
+
+    for (int i = indexOfLastNumber; i < array.length; i++) {
+      numbers.add(array[i]);
+    }
+
+    Integer[] returnArray = new Integer[numbers.size()];
+    numbers.toArray(returnArray);
+
+    return returnArray;
+  }
+
+  public static boolean permutationCheck(String first, String second) {
+    boolean check = true;
+    String longer;
+    String shorter;
+
+    if (first.length() > second.length()) {
+      longer = first;
+      shorter = second;
+    } else {
+      longer = second;
+      shorter = first;
+    }
+
+    for (int i = 0; i < longer.length(); i++) {
+      if (shorter.indexOf(longer.charAt(i)) == -1) {
+        check = false;
+      }
+    }
+    return check;
+  }
+
+  public static String reverse(String inputString) {
+    StringBuilder text = new StringBuilder();
+    for (int i = inputString.length() - 1; i >= 0; i--) {
+      text.append(inputString.charAt(i));
+    }
+    return text.toString();
+  }
+
+  public static void numberCombination() {
+    int number;
+    List<Integer> integerList = new ArrayList<>();
+
+    for (int i = 1; i < 5; i++) {
+      for (int j = 1; j < 5; j++) {
+        for (int k = 1; k < 5; k++) {
+          String numberString = i + String.valueOf(j) + k;
+          number = Integer.parseInt(numberString);
+          if (!integerList.contains(number) && i != j && i != k && k != j) {
+            integerList.add(number);
+            System.out.println(number);
+          }
+        }
+      }
+    }
+    System.out.println(integerList.size());
+  }
+
+  public static void convertSeconds(int seconds) {
+    int h = (int) Math.floor((double) seconds / 3600);
+    int m = (int) Math.floor(((double) seconds - (h * 3600)) / 60);
+    int s = (seconds - ((h * 3600) + (m * 60)));
+    System.out.println(h + ":" + m + ":" + s);
+
+  }
+
+}
