c296af34054c173421c13a1995c689c2a58151a5
maciej <bandurskim@gmail.com>
2020-03-30 16:05:39 +0200

    Added FirstPart.printStaircase method.

--- src/main/java/org/example/FirstPart.java
+++ src/main/java/org/example/FirstPart.java
@@ -9,7 +9,6 @@ import java.util.stream.Collectors;
 public class FirstPart {
 
   public static void main(String[] args) {
-
   }
 
   public static <T, E> T getKeyByValue(Map<T, E> map, E value) {
@@ -185,5 +184,18 @@ public class FirstPart {
       }
     }
     return array;
+  }
+
+  public static String repeat(int count, String with) {
+    return new String(new char[count]).replace("\0", with);
+  }
+
+  public static void printStaircase(int n) {
+
+    for (int i = 0; i < n; i++) {
+      System.out.println(repeat(i, " ") + "_");
+      System.out.println(repeat(i, " ") + " |");
+    }
+
   }
 }
