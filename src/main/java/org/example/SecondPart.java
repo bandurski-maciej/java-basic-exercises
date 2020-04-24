17fba21ad7cf8e0f343e10e41be86fbf0bf16135
maciej <bandurskim@gmail.com>
2020-04-24 17:30:01 +0200

    Added SecondPart.getStartIndicesOfAGivenStringsAnagramsInAnotherGivenString method and test.

--- src/main/java/org/example/SecondPart.java
+++ src/main/java/org/example/SecondPart.java
@@ -1,9 +1,10 @@
 package org.example;
 
 import javax.management.AttributeNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -21,16 +22,57 @@ public class SecondPart {
     LinkedHashMap<String, Long> characterMap = Arrays.stream(s.split(""))
       .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
 
-    String letter = characterMap.entrySet().stream()
-      .filter(stringLongEntry -> stringLongEntry.getValue().equals(1L))
-      .map(Map.Entry::getKey)
-      .findFirst()
-      .orElseThrow(AttributeNotFoundException::new);
-
     return Arrays.stream(s.split(""))
-      .filter(c -> c.equals(letter))
+      .filter(c -> characterMap.get(c) < 2)
       .map(s::indexOf)
       .findFirst()
       .orElseThrow(AttributeNotFoundException::new);
+  }
+
+  public static int getFibonacci(int element) {
+    int a = 0, b = 1, c = 0;
+
+    if (element == 1) {
+      return 1;
+    }
+
+    for (int i = 0; i < element - 1; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+
+    return c;
+  }
+
+  /**
+   * 188. Write a Java program to find all the start indices of a given string's anagrams in another given string.
+   */
+
+  public static List<Integer> getStartIndicesOfAGivenStringsAnagramsInAnotherGivenString(String s, String pattern) {
+    List<String> stringList = Arrays.stream(pattern.split(""))
+      .collect(Collectors.toList());
+    List<String> matchedChars = new ArrayList<>(pattern.length());
+    List<Integer> resultList = new ArrayList<>();
+
+    for (int i = 0; i < s.length(); i++) {
+      String letter = String.valueOf(s.charAt(i));
+
+      if (stringList.contains(letter) && !matchedChars.contains(letter)) {
+        matchedChars.add(letter);
+      } else if (matchedChars.contains(letter)) {
+        matchedChars.clear();
+        matchedChars.add(letter);
+      } else {
+        matchedChars.clear();
+      }
+
+      if (matchedChars.size() == pattern.length()) {
+        resultList.add(i - matchedChars.size() + 1);
+        matchedChars.clear();
+      }
+    }
+
+    return resultList;
   }
 }
--- src/test/java/org/example/SecondPartTest.java
+++ src/test/java/org/example/SecondPartTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import javax.management.AttributeNotFoundException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SecondPartTest {
@@ -11,6 +12,11 @@ public class SecondPartTest {
   @Test
   public void shouldReturnFirstNotRepeatingCharacterInGivenString() throws AttributeNotFoundException {
     assertEquals(SecondPart.getFirstNotRepeatingCharacterInGivenString("google"), 4);
+  }
+
+  @Test
+  public void shouldReturnStartIndicesOfAGivenStringsAnagramsInAnotherGivenString() {
+    assertThat(SecondPart.getStartIndicesOfAGivenStringsAnagramsInAnotherGivenString("zyxwyxyxzwxyz", "xyz")).containsSequence(0, 6, 10);
   }
 
 }
